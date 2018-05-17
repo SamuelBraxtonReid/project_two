c6f28b3de32e44e99c27b77e0478a5869ae7b007
Braxton Reid <reid74@purdue.edu>
2018-05-17 15:51:47 -0400

    change style and clean code

--- index.js
+++ index.js
@@ -1,41 +1,64 @@
+//create planet list
 const planetList = [];
 
 const removePlanet = function(e) {
+  //remove planet
   const button = e.srcElement;
   for (let i = 0; i < planetList.length; i++) {
-    if (planetList[i] === button.id) {
+    if (planetList[i][1] === button.id) {
       planetList.splice(i, 1);
     }
   }
   e.srcElement.parentElement.remove();
 }
 
-function addPlanet(e) {
+const key = function() {
+  //generate unique key
+  const options = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789&|";
+  let k = "";
+  for (let i = 0; i < 16; i++) {
+    k += options.charAt(Math.floor(Math.random() * 64));
+  }
+  return k;
+}
 
+const addPlanet = function(e) {
+
+  //prvent refresh on return
   e.preventDefault();
 
+  //generate unique key
+  const k = key();
+  
+  //get input
   const input = document.querySelector("input");
-  const planets = document.querySelector("#planets");
 
+  //create button
+  const button = document.createElement("button");
+  button.textContent = "X";
+  button.className = "delete";
+  button.id = k;
+
+  //create entry
   const entry = document.createElement("p");
   entry.textContent = input.value;
-  
-  
-  const button = document.createElement("button");
-  button.textContent = "delete";
-  button.className = "delete";
-  button.id = input.value;
-  console.log(planets);
-  planetList.push(input.value);
-  button.addEventListener("click", removePlanet);
-  console.log(button);
-
+  entry.className = "planet";
   entry.appendChild(button);
 
+  //get planets
+  const planets = document.querySelector("#planets");
   planets.appendChild(entry);
 
+  //event listener for delete
+  button.addEventListener("click", removePlanet);
+  
+  //save planet values
+  planetList.push([input.value, k]);
+  
+  //empty input
   input.value = "";
 
 }
 
+//listen for form submit
 document.querySelector("form").addEventListener("submit", addPlanet);
